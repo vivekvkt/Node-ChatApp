dfc617d5d0c8af153d250a8d34a845f9e3c9dfe2
root <root@vindhya.(none)>
2018-04-25 16:01:55 +0530

    firstcommit

--- public/js/index.js
+++ public/js/index.js
@@ -14,10 +14,44 @@ var socket = io();
    });
 
    socket.on('newMessage', function(message){
-   	console.log('newMessage', message);
-   });
+   	console.log('newMessage',message);
+
+
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+
+ });
 
    socket.on('new_email', function(email){
 
       console.log('new eamil',email);
+   });
+
+   // socket.emit('createMessage',{
+
+   //  from:'vivek',
+   //  text:'hi'
+
+   // },function(data){
+
+   // 	console.log("Got it",data);
+   // });
+
+   jQuery('#message-form').on('submit', function(e){
+
+
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+
+      from:'User',
+      text:jQuery('[name=message]').val()
+
+    }, function(){
+
+     
+
+    });
+
    });
--- server/server.js
+++ server/server.js
@@ -48,11 +48,13 @@ io.on('connection', (socket)=>{
 	// });
 
 
-	socket.on('createMessage', (message)=>{
+	socket.on('createMessage', (message, callback)=>{
 
 		console.log('createMessage', message);
 
 		io.emit('newMessage',generateMessage(message.from, message.text));
+
+		callback('this is from server');
 
          // from:message.from,
          // text:message.text,
