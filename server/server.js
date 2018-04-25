2473abe5f0bf24b67ee82b10081f985fb9a6eb14
root <root@vindhya.(none)>
2018-04-25 09:48:26 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
 const path = require('path');
 const express = require('express');
+
+const http = require('http');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
-const port = process.env.PORT || 80;
+
+
+var server = http.createServer(app);
+
+var io = socketIO(server);
+const port = process.env.PORT || 2018;
 
 app.use(express.static(publicPath));
 
 
-app.listen(80, ()=>{
+io.on('connection', (socket)=>{
+
+	console.log('new user connected');
+
+	socket.on('disconnect', ()=>{
+
+    console.log("user was disconnected");
+
+	});
+});
+
+
+server.listen(2018, ()=>{
 
 
 console.log('server running at port${port}');
